71ab0a13e15f642431e40a38cf97623a1e34552b
Bentley Davis <Bentley@BentleyDavis.com>
2017-05-27 11:18:13 -0500

    updae newId and removed old reasonroot non typescript code

--- src/Claim.ts
+++ src/Claim.ts
@@ -38,16 +38,22 @@
 
 type Affects = "AverageTheConfidence" | "MaximumOfConfidence" | "Importance";
 
-// inspired by http://stackoverflow.com/a/27872144/96062
 function newId(): string {
-    var len = 22;
-    var str = "";
-    for (var i = 0; i < len; i++) {
-        var rand = Math.floor(Math.random() * 62);
-        var charCode = rand += rand > 9 ? (rand < 36 ? 55 : 61) : 48;
-        str += String.fromCharCode(charCode);
+    //take the current date and convert to bas 62
+    let decimal = new Date().getTime();
+    let s = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    let result = "";
+    while (decimal >= 1) {
+        result = s[(decimal - (62 * Math.floor(decimal / 62)))] + result;
+        decimal = Math.floor(decimal / 62);
     }
-    return str;
+
+    //Add 5 extra random characters in case multiple ids are creates at the same time
+    result += Array(5).join().split(',').map(function () {
+        return s[(Math.floor(Math.random() * s.length))];
+    }).join('');
+
+    return result
 }
 
 

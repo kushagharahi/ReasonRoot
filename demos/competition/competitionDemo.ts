6dc48c4c08c1c50a20bb473f00df70768339659c
Bentley Davis <Bentley@BentleyDavis.com>
2017-05-31 12:54:52 -0500

    improving the competition demo

--- demos/competition/competitionDemo.ts
+++ demos/competition/competitionDemo.ts
@@ -38,15 +38,6 @@ var competitionDemo: any[] = [
     }, {
         code: (r, d) => {
             d.bag.s3 = d.addClaim("What if we add another reason it could be true?", true);
-            d.bag.s3.weightDif = 1;
-        }, delay: 1000
-    }, {
-        code: (r, d) => {
-            r.mainScore.claim.content = "";
-            return { score: r.mainScore, content: "What do you expect the confidence percentage to be now?" }
-        }, delay: 2000
-    }, {
-        code: (r, d) => {
             r.calculate();
         }, delay: 1000
     }, {
@@ -55,33 +46,20 @@ var competitionDemo: any[] = [
             r.setDisplayState();
             d.addClaim("and a reason it may be false..", false, d.bag.s3).weightDif = 1;
             d.addClaim("What if we add a reason it may be true..", true, d.bag.s3).weightDif = 1;
-        }, delay: 1000
-    }, {
-        code: (r: RRDisplay, d) => {
-            d.bag.s3.claim.content = "";
-            d.bag.s3.weightDif = 0;
-            return { score: d.bag.s3, content: "This claim becomes undecided." }
-            //return { score: d.bag.s3, content: "This statement becomes undecided so it is worth zero points." }
-        }, delay: 2000
-    }, {
-        code: (r, d) => {
             r.calculate();
         }, delay: 1000
     }, {
         code: (r, d) => {
             d.addClaim("Let's add one more reason it may be true", true, d.bag.s3).weightDif = 1;
+            r.calculate();
         }, delay: 1000
     }, {
         code: (r: RRDisplay, d) => {
             d.bag.s3.claim.content = "This claim";
             d.bag.s3.weightDif = 1;
-            return { score: d.bag.s3, content: "This claim now has more reasons to be true." }
-            //return { score: this.bag.s3, content: "Now this statement has 2 purple points minus 1 orange point so the total is 1 point." }
-        }, delay: 2000
-    }, {
-        code: (r, d) => {
             r.calculate();
-        }, delay: 500
+            return { score: d.bag.s3, content: "This claim now has more reasons to be true." }
+        }, delay: 2000
     }, {
         code: (r, d) => {
             r.mainScore.claim.content = "";

5981bded0586db9348e190ead00bc6ce5c70fba2
edgarOPG <eopg95@gmail.com>
2017-06-18 17:52:29 -0600

    Styles are loaded in only one stylesheet file

--- webpack.config.js
+++ webpack.config.js
@@ -4,35 +4,31 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
-  //
   output: {
-    path: path.resolve(__dirname, "/public"),
+    path: path.resolve(__dirname, "/public/"),
     filename: 'bundle.js'
   },
-  //Allows to load a index.html out of root.
+  //Allow us to place index.html in a different position.
   devServer: {
       contentBase: './public/'
   },
   resolve: {
-    alias: {
-      src: path.resolve(__dirname, "/src"),
-    },
     // Add `.ts` and `.tsx` as a resolvable extension.
     extensions: ['.ts', '.tsx', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
   },
   module: {
     loaders: [ // loaders will work with webpack 1 or 2; but will be renamed "rules" in future
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.ts$/, loader: 'ts-loader',
-      //include: [path.resolve(__dirname, '/src/app')]
+      { test: /\.tsx?$/, loader: 'ts-loader'
+      //, include: [path.resolve(__dirname, 'src/app')
     },
-      { test: /\.css$/, loader:'style-loader!css-loader', exclude: /node_modules/},
-      { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader', exclude: /node_modules/}
-      //{ test: /\.scss$  /, loader: ExtractTextPlugin.extract('css!sass') }
+      { test:/\.css$/, loader:'style-loader!css-loader', exclude: /node_modules/},
+      //{ test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader', exclude: /node_modules/}
+      { test: /\.scss$/, loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: [ 'css-loader', 'sass-loader' ] }) }
     ]
   },
-  //plugins: [
-  //    new ExtractTextPlugin("style.css")
-  //],
+  plugins: [
+      new ExtractTextPlugin("style.css")
+  ],
   devtool: 'inline-source-map'
 }

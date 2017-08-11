40dcb1ba146b744b9039d056941a26777317a9ec
Kusha Gharahi <kusha95@gmail.com>
2017-08-11 17:43:48 -0500

    Fixed webpack transpilation

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './dist/index.js',
+  entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, "/public/"),
     filename: 'bundle.js',
@@ -16,11 +16,9 @@ module.exports = {
     extensions: ['.ts', '.tsx', '.js'] // note if using webpack 1 you'd also need a '' in the array as well
   },
   module: {
-    loaders: [ // loaders will work with webpack 1 or 2; but will be renamed "rules" in future
+    rules: [ // loaders will work with webpack 1 or 2; but will be renamed "rules" in future
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: 'ts-loader', exclude: path.resolve(__dirname, '/node_modules'),
-      include: path.resolve(__dirname, '/src/app')
-    },
+      { test: /\.ts?$/, loader: 'ts-loader', exclude: path.resolve(__dirname, '/node_modules')},
       { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader', exclude: path.resolve(__dirname, '/node_modules')}
     ]
   },
